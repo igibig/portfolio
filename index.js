cc13545328ef879fa224d28e526932868a4feeb6
igibiLux <123431951+igibiLux@users.noreply.github.com>
2025-07-07 06:57:15 +0300

    added local time getter

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("Hi there!");
+const YourCurrentTime = document.getElementById("timeForYou");
+const offset = new Date().getTimezoneOffset() / 60;
+
+YourCurrentTime.textContent = offset < 0 ? `${9 + Math.abs(offset)}:00` : `0${9 - offset}:00`;
